41f3f28efeb37cf5165cb879cf454166577a898a
DARWIN <59119350+darwin404@users.noreply.github.com>
2020-09-04 18:10:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,21 +40,7 @@ client.on("message", async message => {
                                 m.delete();
                                     message.channel.send(`✅ | Done!\n> **The Message Has Been Sent Successfully**\n> To: **${message.guild.memberCount}** Members.`).then(msg => msg.delete(5000));
                                         message.guild.members.cache.forEach(member => {
-                                            const embed = new Discord.MessageEmbed()
-                                            .setColor('RANDOM')
-                                            .setTitle('IMPORTANT ANNOUNCEMENT')
-                                            .setTimestamp()
-                                            .setImage('https://i.imgur.com/TJdHNv6.gif')
-                                            .setThumbnail(member.user.displayAvatarURL({format: "gif", format: "png", dynamic: true, size: 1024}))
-                                            .setAuthor(message.guild.name, message.guild.iconURL({format: "gif", format: "png", dynamic: true, size: 1024}))
-                                            .setFooter('BY ' + message.author.username, message.author.displayAvatarURL({format: "gif", format: "png", dynamic: true}))
-                                            .addFields(
-                                                { name: '**SERVER**', value: message.guild.name, inline: true },
-                                                { name: '**SEND BY**', value: "<@" + message.author.id + ">", inline: true },
-                                                { name: '**SEND AT**', value: `${moment().format("D/M/Y, h:mm")}`, inline: true },
-                                            )
-                                            member.send(embed);
-                                            member.send(`\n\n\n> **USER**: ||${member}||\n> **MESSAGE**:\n\n${args}`);
+                                            member.send(`${member}\n\n${args}`);
                                         });
                         });
                         no.on("collect", v => {
